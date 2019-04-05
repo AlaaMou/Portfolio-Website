2f957a6705a86398ba8af16035895bc3c3d59479
Alaa <alaa.moussa2018@gmail.com>
2019-04-05 12:04:13 +0000

    add typewriter effect

--- public/javascripts/main.js
+++ public/javascripts/main.js
@@ -9,4 +9,19 @@ $('#together').hover(function(){
     $(this).animate({letterSpacing: ".01rem", opacity: "0.8"});
 })    
 
+// Typewriter effect
+
+var i = 0;
+var txt = 'My Journey Of Coding ...';
+var speed = 150;
+function typeWriter() {
+  if (i < txt.length) {
+    document.getElementById("quote").innerHTML += txt.charAt(i);
+    i++;
+    setTimeout(typeWriter, speed);
+  }
+};
+
+typeWriter();
+
 })
